c33142cd9afaa35662235dab0dce7cb3d7e91604
fringemonkey89 <markomajeticjr@gmail.com>
2023-11-05 11:15:12 -0500

    added peer dependenc

---
+++ public/script.js
@@ -0,0 +1,10 @@
+const socket = io('/')
+const myPeer = new PerformanceEntry(undefined, {
+    host: '/',
+    port: '3001'
+})
+socket.emit('join-room', ROOM_ID, 10)
+
+socket.on('user-connected', userId => {
+    console.log('User connected' + userId)
+})
